c2489d0f86584e718b8d0b6fe10cd04a5476e8a8
developer119 <developer119korea@gmail.com>
2018-08-20 17:31:42 +0900

    페이징 버그를 수정
    게시물수를 파악하여 정확하게 페이징을 할수 있도록 수정

--- routes/posts.js
+++ routes/posts.js
@@ -9,10 +9,8 @@ router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/pasing/:cur", function (req, res) {
 
-    var page_size = 10;
-    var page_list_size = 10;
-    var no = "";
-    var totalContentCount = 0;
+    var max_size_view_page = 10; // 한번에 보여줄 수 있는 최대 페이지 목록 수
+    var max_size_view_content = 10; // 한 페이지에서 보여줄 수 있는 최대 게시글 수
 
     var queryString = 'select count(*) as cnt from Contents'
     getConnection().query(queryString, function (error2, data) {
@@ -20,38 +18,32 @@ router.get("/pasing/:cur", function (req, res) {
             console.log(error2 + "메인화면 mysql 조회 실패");
             return;
         }
-        console.log('data ' + data[0].cnt);
 
-        totalContentCount = data[0].cnt;
-
+        var total_size_content = data[0].cnt;  // 전체 게시글의 수
         var curPage = req.params.cur;
-
-        console.log("cur Page : " + curPage, "total Page : " + totalContentCount);
-
-        if (totalContentCount < 0) {
-            totalContentCount = 0;
+        if (total_size_content < 0) {
+            total_size_content = 0;
         }
 
-        var totalPage = Math.ceil(totalContentCount / page_size);
-        var totalSet = Math.ceil(totalContentCount / page_list_size);
-        var curSet = Math.ceil(curPage / page_list_size);
-        var startPage = ((curSet - 1) * 10) + 1;
-        var endPage = (startPage + page_list_size) - 1;
+        var totalPage = parseInt(total_size_content / max_size_view_content) + 1; // 필요한 전체 페이지 수
+        var totalSet = parseInt(totalPage / max_size_view_page) + 1; // 필요한 전체 세트 수
+        var curSet = parseInt(curPage / max_size_view_page) + 1;
+        var startPage = ((curSet - 1) * max_size_view_page) + 1;
+        var min_size_page = Math.min(totalPage, max_size_view_page);
+        var endPage = (startPage + min_size_page) - 1;
 
-        console.log('[0] curPage : ' + curPage + ' | [1] page_list_size : ' + page_list_size + ' | [2] page_size : ' + page_size + ' | [3] totalPage : ' + totalPage + ' | [4] totalSet : ' + totalSet + ' | [5] curSet : ' + curSet + ' | [6] startPage : ' + startPage + ' | [7] endPage : ' + endPage);
-
+        var no = 0; // 보여줄 게시글의 시작 번호
         if (curPage < 0) {
             no = 0;
         } else {
-            no = (curPage - 1) * 10
+            no = (curPage - 1) * max_size_view_content
         }
 
         var result2 = {
             "curPage": curPage,
-            "page_list_size": page_list_size,
-            "page_size": page_size,
-            "totalPage:": totalPage,
-            "totalSet:": totalSet,
+            "max_size_view_content": max_size_view_content,
+            "totalPage": totalPage,
+            "totalSet": totalSet,
             "curSet": curSet,
             "startPage": startPage,
             "endPage": endPage
@@ -64,7 +56,7 @@ router.get("/pasing/:cur", function (req, res) {
             }
 
             var queryString = 'select * from Contents order by id desc limit ?,?';
-            getConnection().query(queryString, [no, page_size], function (error, result) {
+            getConnection().query(queryString, [no, max_size_view_content], function (error, result) {
                 if (error) {
                     console.log("페이징 에러");
                     return;
@@ -89,8 +81,6 @@ router.get("/insert", function (req, res) {
 router.post("/insert", function (req, res) {
     console.log("삽입 포스트 데이터 진행")
     var body = req.body;
-    console.log('[0] id : ' + body.id + ' | [1] title : ' + body.title + ' | [2] writer : ' + body.writer + ' | description : ' + body.description);
-    console.log();
     getConnection().query('insert into Contents(title, writer, description) values (?,?,?)', [body.title, body.writer, body.description], function () {
         //응답
         res.redirect('/main');
